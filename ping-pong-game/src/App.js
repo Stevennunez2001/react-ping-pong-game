790a1a174e1876d1bd50f77795af8ea4fd6b721d
Steven Nunez <stevenelpocoamigo@gmail.com>
2024-07-12 19:31:30 -0400

    final styling

--- ping-pong-game/src/App.js
+++ ping-pong-game/src/App.js
@@ -19,18 +19,7 @@ const App = () => {
 
   
   useEffect(() => {
-    if(!gameRunning){
-      const handleKeyPress = (e) => {
-      switch (e.code){
-        case 'Enter':
-              setGameRunning(true);
-              break;
-          default:
-            break;
-      }
-      }
-    }
-    else {
+    if(gameRunning){
       const handleKeyPress = (e) => {
         switch (e.code) {
           case 'ArrowUp':
